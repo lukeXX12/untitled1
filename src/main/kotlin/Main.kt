52df3ba310269998b9f39d5d977c24b8f4f7d4d5
lukeXX12 <92647553+lukeXX12@users.noreply.github.com>
2021-12-19 00:22:05 +0400

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,157 @@
+
+
+//დავალება 1
+//fun main(){
+//    print("დაწერეთ სიტყვა")
+//val chawere= readln()
+//    isPalindrome(chawere)
+//}
+// fun isPalindrome(text: String ) {
+//    if (text.reversed() == text) {
+//        print("Text is palindrome")
+//    }
+//    else {
+//         println("text is not palindrome")
+//    }
+//}
+
+//დავალება 2
+//fun main (){
+//    var tetri = readln().toInt()
+//    daxurdaveba(tetri)
+//}
+//fun daxurdaveba( tetri: Int){
+//    var mRaod = 0
+//    var tetrii = tetri
+//
+//    for (i in 0..tetrii){
+//        if (tetrii >=50 ){
+//            mRaod +=1
+//            tetrii =tetrii - 50
+//        }
+//        else if (tetrii >=20){
+//            mRaod +=1
+//            tetrii = tetrii- 20
+//        }
+//        else if (tetrii >= 10){
+//            tetrii = tetrii - 10
+//            mRaod +=1
+//        }
+//        else if (tetrii >= 5){
+//            tetrii = tetrii - 5
+//            mRaod +=1
+//        }
+//        else if (tetrii >= 1){
+//            tetrii = tetrii - 1
+//            mRaod += 1
+//        }
+//    }
+//    println(mRaod)
+//}
+
+
+//დავალება 3
+
+//fun main(){
+//val masivi: Array<Int> = arrayOf(6, 3, 2, 5, 10, -3, 1)
+//
+//    ricxvi(masivi)
+//
+//}
+//fun ricxvi (masivi: Array<Int>){
+//    val mSigrdz = masivi.size
+//
+//    for (i in 1..mSigrdz ){
+//        if(!!masivi.contains(i)){
+//
+//        }
+//        else{
+//            print(i)
+//            break
+//        }
+//    }
+//}
+
+
+
+//davaleba 4
+//fun main() {
+//    var stringi = "()()()()()()()()"
+//    parentheseChecker(stringi)
+//
+//
+//
+//
+//}
+//fun parentheseChecker(stringi: String) {
+//    var openF = 0
+//    var closedF = 0
+//    for (i in 0..stringi.length) {
+//        val gaxsna = stringi.take(i)
+//        if (gaxsna == "(") {
+//            openF += 1
+//        } else {
+//            closedF += 1
+//
+//
+//        }
+//    }
+//    if (openF == closedF) {
+//        print("ფრჩხილები სწორადაა")
+//
+//
+//    }
+//    else{
+//        print("frCxilebi arasworadaa")
+//    }
+//}
+
+//davaleba 5
+//fun main(args: Array<String>){
+//    print("sheiyvanet kibeebis raodenoba")
+//    val n_kibe = readln().toLong()
+//    var t1 = 0
+//    var t2 = 1
+//    val list: MutableList<Long> = mutableListOf()
+//
+//    for (i in 0 ..n_kibe+1){
+//        list.add(t1.toLong())
+//        val sum = t1+t2
+//        t1=t2
+//        t2=sum
+//
+//    }
+//
+//    print(list.last())
+//
+//
+////😭😭😭😭😭
+//}
+////😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//დავალება 6   -
+
+
+
+
+
+
+
+
